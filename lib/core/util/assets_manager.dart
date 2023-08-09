ba2286f185ce0d91a631fe78de7b89351b8a8145
Momen Muhammad <momenmuhammad01@gmail.com>
2023-08-09 12:06:54 +0200

    Added new assets

--- lib/core/util/assets_manager.dart
+++ lib/core/util/assets_manager.dart
@@ -1,5 +1,7 @@
 class AssetsManager{
   static const String primaryFontFamily = 'Montserrat';
   static const String secondaryFontFamily = 'DM Serif Display';
+  static const String appLogo = 'assets/images/app_logo.png';
+  static const String welcomeImage = 'assets/images/welcome_image.jpeg';
   static const String offersPic = 'https://images.unsplash.com/photo-1599309329365-0a9ed45a1da3?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=687&q=80';
 }
