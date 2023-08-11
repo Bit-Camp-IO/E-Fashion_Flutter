3d2bd8f7b304815989c9959adaa1d4f28d811cfc
Momen Muhammad <momenmuhammad01@gmail.com>
2023-08-11 09:43:37 +0200

    Filters UI Impl

---
+++ lib/features/home/presentation/components/titled_avatar.dart
@@ -0,0 +1,71 @@
+import 'package:efashion_flutter/core/util/colors_manager.dart';
+import 'package:flutter/material.dart';
+
+class TitledAvatar extends StatefulWidget {
+  const TitledAvatar({
+    super.key,
+    required this.imagePath,
+    required this.title,
+    required this.onTap,
+  });
+
+  final String imagePath;
+  final String title;
+  final void Function() onTap;
+
+  @override
+  State<TitledAvatar> createState() => _TitledAvatarState();
+}
+
+class _TitledAvatarState extends State<TitledAvatar> {
+  bool isTapped = false;
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: () {
+        setState(() {
+          isTapped = !isTapped;
+        });
+        widget.onTap();
+      },
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          _buildAvatarImage(),
+          Text(
+            widget.title,
+            style: Theme.of(context).textTheme.bodyMedium,
+          )
+        ],
+      ),
+    );
+  }
+
+  _buildAvatarImage() {
+    return Stack(
+      alignment: Alignment.center,
+      children: [
+        AnimatedOpacity(
+          opacity: isTapped ? 0.0 : 1.0,
+          duration: const Duration(milliseconds: 300),
+          curve: Curves.easeInOut,
+          child: CircleAvatar(
+            backgroundImage: NetworkImage(widget.imagePath),
+          ),
+        ),
+        AnimatedOpacity(
+          opacity: isTapped ? 1.0 : 0.0,
+          duration: const Duration(milliseconds: 300),
+          curve: Curves.easeInOut,
+          child: CircleAvatar(
+            backgroundColor: ColorsManager.lightPrimaryColor,
+            radius: 23,
+            child: CircleAvatar(
+              backgroundImage: NetworkImage(widget.imagePath),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
