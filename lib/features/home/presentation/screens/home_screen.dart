2c675ed8a5b4590e97b7f6f48aaea9276fcec9da
Momen Muhammad <momenmuhammad01@gmail.com>
2023-08-11 09:44:15 +0200

    Home Ui Impl

--- lib/features/home/presentation/screens/home_screen.dart
+++ lib/features/home/presentation/screens/home_screen.dart
@@ -1,5 +1,10 @@
 import 'package:auto_route/auto_route.dart';
+import 'package:efashion_flutter/features/home/presentation/components/collection_carousel_component.dart';
+import 'package:efashion_flutter/features/home/presentation/components/filter_component.dart';
+import 'package:efashion_flutter/features/home/presentation/components/offers_carousel.dart';
+import 'package:efashion_flutter/features/home/presentation/components/section_widget.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 @RoutePage()
 class HomeScreen extends StatelessWidget {
@@ -7,12 +12,32 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text(
-          'Home Screen',
+    return ListView(
+      padding: EdgeInsets.zero,
+      children: [
+        const CollectionCarouselComponent(),
+        SizedBox(height: 20.h),
+        SectionWidget(
+          sectionTitle: 'Filters',
+          sectionButtonTitle: 'See All',
+          onTap: () {},
         ),
-      ),
+        const FilterComponent(),
+        SizedBox(height: 10.h),
+        SectionWidget(
+          sectionTitle: 'Offers',
+          sectionButtonTitle: 'See All',
+          onTap: () {},
+        ),
+        const OffersCarousel(),
+        SizedBox(height: 10.h),
+        SectionWidget(
+          sectionTitle: 'Zara',
+          sectionButtonTitle: 'See All',
+          onTap: () {},
+        ),
+        SizedBox(height: 100.h),
+      ],
     );
   }
 }
