e5f27fcd56dfa9b55329ddd2b4d11b3521fcbd3d
Momen Muhammad <momenmuhammad01@gmail.com>
2023-08-11 09:53:12 +0200

    added a new color

--- lib/core/util/colors_manager.dart
+++ lib/core/util/colors_manager.dart
@@ -3,4 +3,5 @@ import 'package:flutter/material.dart';
 class ColorsManager{
   static const Color lightPrimaryColor = Color(0xFF00629D);
   static const Color darkPrimaryColor = Color(0xFF67B1F4);
+  static const Color surfaceContainerHigh = Color(0xFFE8E8EB);
 }
