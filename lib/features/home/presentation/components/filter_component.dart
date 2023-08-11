677f3483f1b2744b7d8ca37f86af3efe7a64bc69
Momen Muhammad <momenmuhammad01@gmail.com>
2023-08-11 09:43:37 +0200

    Filters UI Impl

--- lib/features/home/presentation/components/filter_component.dart
+++ lib/features/home/presentation/components/filter_component.dart
@@ -1,11 +1,9 @@
 import 'package:efashion_flutter/core/util/colors_manager.dart';
-import 'package:efashion_flutter/features/home/presentation/components/section_widget.dart';
 import 'package:efashion_flutter/features/home/presentation/components/titled_avatar.dart';
 import 'package:efashion_flutter/features/home/presentation/mock/filter_mock.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:iconsax/iconsax.dart';
 
 class FilterComponent extends StatefulWidget {
   const FilterComponent({super.key});
@@ -18,8 +16,6 @@ class _FilterComponentState extends State<FilterComponent> {
   late final ScrollController _filterController;
   double animatedFilterWidth = 145;
   bool isFilterOn = false;
-  List selectedFilters = [];
-  List selectedGenders = [];
 
   @override
   void initState() {
@@ -51,165 +47,117 @@ class _FilterComponentState extends State<FilterComponent> {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        SizedBox(
-          height: 40.h,
-          child: Row(
-            children: [
-              const SectionWidget(
-                sectionTitle: 'Filters',
-              ),
-              const Spacer(flex: 1),
-              AnimatedOpacity(
-                opacity:  selectedGenders.isNotEmpty || selectedFilters.isNotEmpty ? 1.0 : 0.0,
-                duration: const Duration(milliseconds: 300),
-                curve: Curves.easeInOut,
-                child: Padding(
-                  padding: const EdgeInsets.only(right: 8.0),
-                  child: InkWell(
-                    onTap: () {},
+    return  Padding(
+      padding: const EdgeInsets.only(left: 16.0),
+      child: SizedBox(
+        height: 84.h,
+        child: ListView(
+          scrollDirection: Axis.horizontal,
+          controller: _filterController,
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(right: 16.0).r,
+              child: Row(
+                children: [
+                  Visibility(
+                    visible: false,
                     child: Container(
-                      width: 80.w,
+                      width: 40.w,
                       height: 40.h,
                       decoration: const BoxDecoration(
+                        shape: BoxShape.circle,
                         color: Color(0xFF526070),
-                        borderRadius: BorderRadius.all(
-                          Radius.circular(20),
-                        ),
                       ),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          const Icon(Iconsax.filter, color: Colors.white, size: 16),
-                          Text(
-                            'Apply',
-                            style: Theme.of(context)
-                                .textTheme
-                                .labelMedium!
-                                .copyWith(color: Colors.white),
-                          ),
-
-                        ],
+                      child: Center(
+                        child: Text(
+                          'Go',
+                          style: Theme.of(context)
+                              .textTheme
+                              .bodyMedium!
+                              .copyWith(color: Colors.white),
+                        ),
                       ),
                     ),
                   ),
-                ),
-              )
-            ],
-          ),
-        ),
-        SizedBox(height: 8.h),
-        SizedBox(
-          height: 84.h,
-          child: ListView(
-            scrollDirection: Axis.horizontal,
-            controller: _filterController,
-            children: [
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 16).r,
-                child: Row(
-                  children: [
-                    AnimatedContainer(
-                      duration: const Duration(milliseconds: 1000),
-                      curve: Curves.easeInOut,
-                      width: animatedFilterWidth.w,
-                      height: 84.h,
-                      decoration: const BoxDecoration(
-                        color: Color(0xFFE8E8EB),
-                        borderRadius: BorderRadius.all(
-                          Radius.circular(15),
-                        ),
+                  AnimatedContainer(
+                    duration: const Duration(milliseconds: 1000),
+                    curve: Curves.easeInOut,
+                    width: animatedFilterWidth.w,
+                    height: 84.h,
+                    decoration: const BoxDecoration(
+                      color: Color(0xFFE8E8EB),
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(15),
                       ),
-                      child: SingleChildScrollView(
-                        physics: const NeverScrollableScrollPhysics(),
-                        scrollDirection: Axis.horizontal,
-                        child: SizedBox(
-                          width: 145.w,
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                            children: List.generate(
-                              genders.length,
-                              (genderIndex) => TitledAvatar(
-                                onTap: () {
-                                  setState(() {
-                                    if (selectedGenders
-                                        .contains(genders[genderIndex].name)) {
-                                      selectedGenders.removeWhere((element) =>
-                                          element == genders[genderIndex].name);
-                                    } else {
-                                      selectedGenders.add(genders[genderIndex].name);
-                                    }
-                                  });
-                                },
-                                imagePath: genders[genderIndex].picture,
-                                title: genders[genderIndex].name,
-                              ),
+                    ),
+                    child: SingleChildScrollView(
+                      physics: const NeverScrollableScrollPhysics(),
+                      scrollDirection: Axis.horizontal,
+                      child: SizedBox(
+                        width: 145.w,
+                        child: Row(
+                          mainAxisAlignment:
+                          MainAxisAlignment.spaceEvenly,
+                          children: List.generate(
+                            genders.length,
+                                (index) => TitledAvatar(
+                              onTap: () {},
+                              imagePath: genders[index].picture,
+                              title: genders[index].name,
                             ),
                           ),
                         ),
                       ),
                     ),
-                    SizedBox(width: 14.w),
-                    Container(
-                      width: 430.w,
-                      height: 84.h,
-                      decoration: const BoxDecoration(
-                        color: Color(0xFFE8E8EB),
-                        borderRadius: BorderRadius.all(
-                          Radius.circular(15),
-                        ),
+                  ),
+                  SizedBox(width: 14.w),
+                  Container(
+                    width: 430.w,
+                    height: 84.h,
+                    decoration: const BoxDecoration(
+                      color: Color(0xFFE8E8EB),
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(15),
                       ),
-                      child: Row(
-                        children: [
-                          Container(
-                            width: 6.w,
-                            height: 42.h,
-                            decoration: const BoxDecoration(
+                    ),
+                    child: Row(
+                      children: [
+                        Container(
+                          width: 6.w,
+                          height: 42.h,
+                          decoration: const BoxDecoration(
                               color: ColorsManager.lightPrimaryColor,
                               borderRadius: BorderRadius.only(
                                 topRight: Radius.circular(5),
                                 bottomRight: Radius.circular(5),
+                              )),
+                        ),
+                        SizedBox(
+                          width: 420.w,
+                          child: Row(
+                            mainAxisAlignment:
+                            MainAxisAlignment.spaceEvenly,
+                            children: List.generate(
+                              filters.length,
+                                  (index) => TitledAvatar(
+                                onTap: () {},
+                                imagePath: filters[index].filterImageUrl,
+                                title: filters[index].filterName,
                               ),
                             ),
                           ),
-                          SizedBox(
-                            width: 420.w,
-                            child: Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                              children: List.generate(
-                                filters.length,
-                                (filterIndex) => TitledAvatar(
-                                  onTap: () {
-                                    setState(() {
-                                      if (selectedFilters.contains(filters[filterIndex].filterName)) {
-                                        selectedFilters.removeWhere((element) =>
-                                            element == filters[filterIndex].filterName);
-                                      } else {
-                                        selectedFilters
-                                            .add(filters[filterIndex].filterName);
-                                      }
-                                    });
-                                  },
-                                  imagePath: filters[filterIndex].filterImageUrl,
-                                  title: filters[filterIndex].filterName,
-                                ),
-                              ),
-                            ),
-                          ),
-                        ],
-                      ),
+                        ),
+                      ],
                     ),
-                  ],
-                ),
-              )
-            ],
-          ),
+                  ),
+                ],
+              ),
+            )
+          ],
         ),
-      ],
+      ),
     );
   }
-
   @override
   void dispose() {
     _filterController
--- lib/features/home/presentation/components/titled_avatar.dart
+++ lib/features/home/presentation/components/titled_avatar.dart
@@ -18,13 +18,13 @@ class TitledAvatar extends StatefulWidget {
 }
 
 class _TitledAvatarState extends State<TitledAvatar> {
-  bool isSelected = false;
+  bool isTapped = false;
   @override
   Widget build(BuildContext context) {
     return InkWell(
       onTap: () {
         setState(() {
-          isSelected = !isSelected;
+          isTapped = !isTapped;
         });
         widget.onTap();
       },
@@ -46,7 +46,7 @@ class _TitledAvatarState extends State<TitledAvatar> {
       alignment: Alignment.center,
       children: [
         AnimatedOpacity(
-          opacity: isSelected ? 0.0 : 1.0,
+          opacity: isTapped ? 0.0 : 1.0,
           duration: const Duration(milliseconds: 300),
           curve: Curves.easeInOut,
           child: CircleAvatar(
@@ -54,7 +54,7 @@ class _TitledAvatarState extends State<TitledAvatar> {
           ),
         ),
         AnimatedOpacity(
-          opacity: isSelected ? 1.0 : 0.0,
+          opacity: isTapped ? 1.0 : 0.0,
           duration: const Duration(milliseconds: 300),
           curve: Curves.easeInOut,
           child: CircleAvatar(
