2faa5f2c2f8f94f49fed2102beb911f95a934902
Momen Muhammad <momenmuhammad01@gmail.com>
2023-08-12 08:02:51 +0200

    Brand component UI impl

--- lib/features/home/presentation/components/brand_component.dart
+++ lib/features/home/presentation/components/brand_component.dart
@@ -1,6 +1,5 @@
 import 'package:efashion_flutter/features/home/presentation/components/brand_card.dart';
-import 'package:efashion_flutter/features/home/presentation/components/cart_bottom_sheet.dart';
-import 'package:efashion_flutter/features/home/presentation/mock/product_mock.dart';
+import 'package:efashion_flutter/features/home/presentation/mock/brand_mock.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
@@ -12,6 +11,7 @@ class BrandComponent extends StatefulWidget {
 }
 
 class _BrandComponentState extends State<BrandComponent> {
+
   @override
   Widget build(BuildContext context) {
     return SizedBox(
@@ -24,28 +24,14 @@ class _BrandComponentState extends State<BrandComponent> {
           return Padding(
             padding: const EdgeInsets.only(left: 8.0).r,
             child: BrandCard(
-              productImage: brandProducts[index].productImage,
+              productImage: brandProducts[index].productImagePath,
               productName: brandProducts[index].productName,
               productPrice: brandProducts[index].productPrice,
               isFavorite: brandProducts[index].isFavorite,
-              onCartTap: () {
-                showModalBottomSheet(
-                  context: context,
-                  builder: (context) {
-                    return CartBottomSheet(
-                      productName: brandProducts[index].productName,
-                      productPrice: brandProducts[index].productPrice,
-                      productColors: brandProducts[index].productColors,
-                      productSizes: brandProducts[index].productSizes,
-                      productPieces: brandProducts[index].avaliblePieces,
-                    );
-                  },
-                );
-              },
+              onCartTap: () {},
               onFavoriteTap: () {
                 setState(() {
-                  brandProducts[index].isFavorite =
-                      !brandProducts[index].isFavorite;
+                  brandProducts[index].isFavorite = !brandProducts[index].isFavorite;
                 });
               },
             ),
@@ -55,3 +41,5 @@ class _BrandComponentState extends State<BrandComponent> {
     );
   }
 }
+
+
