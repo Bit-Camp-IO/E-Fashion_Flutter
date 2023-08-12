51f8259c116a794f55d9b5589e0ed1e2336ace04
Abdullah-3hmed <abdullhahmed107@gmail.com>
2023-08-12 15:39:24 +0300

    apply curve and blur in welcome screen

--- lib/features/auth/presentation/components/welcome_screen_body.dart
+++ lib/features/auth/presentation/components/welcome_screen_body.dart
@@ -1,14 +1,15 @@
 import 'dart:ui';
 
 import 'package:efashion_flutter/core/util/assets_manager.dart';
+import 'package:efashion_flutter/features/auth/presentation/components/welcome_screen_custom_clip_path.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class WelcomeScreenBody extends StatelessWidget {
   const WelcomeScreenBody({super.key});
+
   @override
   Widget build(BuildContext context) {
-    debugPrint(MediaQuery.sizeOf(context).width.toString());
     return Stack(
       children: [
         Image.asset(
@@ -37,14 +38,9 @@ class WelcomeScreenBody extends StatelessWidget {
                     clipper: CustomClipPath(),
                     child: ClipRect(
                       child: BackdropFilter(
-                        filter: ImageFilter.blur(sigmaX: 5, sigmaY: 5),
+                        filter: ImageFilter.blur(sigmaX: 50, sigmaY: 50),
                         child: Container(
-                          decoration: BoxDecoration(
-                            color: const Color(0xFF001D34).withOpacity(.5),
-                            borderRadius: BorderRadius.only(
-                              topLeft: const Radius.circular(40.0).r,
-                            ),
-                          ),
+                          color: const Color(0xFF001D34).withOpacity(.5),
                         ),
                       ),
                     ),
@@ -80,16 +76,17 @@ class WelcomeScreenBody extends StatelessWidget {
                     ),
                   ),
                   Positioned(
-                      left: 273.w,
-                      bottom: 142.h,
-                      child: InkWell(
-                        onTap: () {},
-                        child: const Image(
-                          image: AssetImage(
-                            AssetsManager.swipeUpImage,
-                          ),
+                    left: 273.w,
+                    bottom: 142.h,
+                    child: InkWell(
+                      onTap: () {},
+                      child: const Image(
+                        image: AssetImage(
+                          AssetsManager.swipeUpImage,
                         ),
-                      )),
+                      ),
+                    ),
+                  ),
                 ],
               ),
             ),
@@ -97,28 +94,5 @@ class WelcomeScreenBody extends StatelessWidget {
         ),
       ],
     );
-  }
-}
-
-// path.lineTo(0, height);
-// path.lineTo(width, height);
-// path.lineTo(width, 100);
-class CustomClipPath extends CustomClipper<Path> {
-  @override
-  Path getClip(Size size) {
-    double width = size.width;
-    double height = size.height;
-    final path = Path();
-    path.lineTo(0, height); // point 1
-    path.lineTo(width, height); // point 2
-    path.lineTo(width, 0); // point 3
-    path.quadraticBezierTo(width * .5, height - 100, width, 0);
-    path.close();
-    return path;
-  }
-
-  @override
-  bool shouldReclip(covariant CustomClipper<Path> oldClipper) {
-    return false;
   }
 }
