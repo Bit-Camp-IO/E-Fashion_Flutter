e87f11a412bb38cd95fbbde0d10cfa7f27cd507a
Momen Muhammad <momenmuhammad01@gmail.com>
2023-08-13 19:04:22 +0200

    Home Screen UI Impl

---
+++ lib/features/home/presentation/components/product_size.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class ProductSize extends StatelessWidget {
+  const ProductSize({
+    super.key,
+    required this.size,
+    required this.onTap,
+    required this.isSelected,
+  });
+
+  final String size;
+  final void Function() onTap;
+  final bool isSelected;
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: onTap,
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Stack(
+          alignment: Alignment.center,
+          children: [
+            AnimatedOpacity(
+              opacity: isSelected ? 0.0 : 1.0,
+              duration: const Duration(milliseconds: 300),
+              curve: Curves.easeInOut,
+              child: Text(
+                size,
+                style: Theme.of(context).textTheme.labelMedium,
+              ),
+            ),
+            AnimatedOpacity(
+              opacity: isSelected ? 1.0 : 0.0,
+              duration: const Duration(milliseconds: 300),
+              curve: Curves.easeInOut,
+              child: Container(
+                width: 24.w,
+                height: 24.h,
+                decoration: BoxDecoration(
+                    color: Theme.of(context).primaryColor,
+                    shape: BoxShape.circle
+                ),
+                child: Center(
+                  child: Text(
+                    size,
+                    style: Theme.of(context).textTheme.labelMedium!.copyWith(
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
