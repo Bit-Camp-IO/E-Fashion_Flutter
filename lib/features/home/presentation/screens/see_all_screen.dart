2025d60c3b89bfea4d79b5a8ab548d356061ee2d
Momen Muhammad <momenmuhammad01@gmail.com>
2023-08-13 19:04:22 +0200

    Home Screen UI Impl

--- lib/features/home/presentation/screens/see_all_screen.dart
+++ lib/features/home/presentation/screens/see_all_screen.dart
@@ -1,63 +1,17 @@
 import 'package:auto_route/auto_route.dart';
-import 'package:efashion_flutter/features/home/presentation/mock/product_mock.dart';
 import 'package:flutter/material.dart';
 
 @RoutePage()
-class SeeAllScreen extends StatelessWidget {
+class SeeAllScreen extends StatefulWidget {
   const SeeAllScreen({Key? key}) : super(key: key);
 
   @override
+  State<SeeAllScreen> createState() => _SeeAllScreenState();
+}
+
+class _SeeAllScreenState extends State<SeeAllScreen> {
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: GridView.builder(
-        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: 2, // Number of columns in the grid
-        ),
-        itemCount: brandProducts.length,
-        itemBuilder: (context, index) {
-          if(index == 0){
-            return Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Container(
-                color: Colors.red,
-                child: Center(
-                  child: Text('Item $index - Custom Style'),
-                ),
-              ),
-            );
-          } else if (index == 1) {
-            return Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Container(
-                color: Colors.orange,
-                child: Center(
-                  child: Text('Item $index - Custom Style'),
-                ),
-              ),
-            );
-          } else if (index % 2 == 0) {
-            return Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Container(
-                color: Colors.blue,
-                child: Center(
-                  child: Text('Item $index - Style 1'),
-                ),
-              ),
-            );
-          } else {
-            return Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Container(
-                color: Colors.green,
-                child: Center(
-                  child: Text('Item $index - Style 2'),
-                ),
-              ),
-            );
-          }
-        },
-      ),
-    );
+    return Container();
   }
 }
