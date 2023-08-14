c071893ad61438d8b09ce1f9db662d21084d6a24
Momen Muhammad <momenmuhammad01@gmail.com>
2023-08-14 12:25:30 +0200

    Add See All Screen

--- lib/config/router/app_router.dart
+++ lib/config/router/app_router.dart
@@ -1,19 +1,11 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:efashion_flutter/features/account/presentation/screens/account_screen.dart';
-import 'package:efashion_flutter/features/auth/presentation/screens/login_screen.dart';
-import 'package:efashion_flutter/features/auth/presentation/screens/sign_up_screen.dart';
 import 'package:efashion_flutter/features/auth/presentation/screens/splash_screen.dart';
-import 'package:efashion_flutter/features/auth/presentation/screens/welcome_screen.dart';
 import 'package:efashion_flutter/features/cart/presentation/screens/cart_screen.dart';
 import 'package:efashion_flutter/features/favorite/presentation/screens/favorite_screen.dart';
-import 'package:efashion_flutter/features/home/presentation/screens/add_review_screen.dart';
-import 'package:efashion_flutter/features/home/presentation/screens/all_reviews_screen.dart';
 import 'package:efashion_flutter/features/home/presentation/screens/home_screen.dart';
 import 'package:efashion_flutter/features/bottom_nav_bar/bottom_nav_bar.dart';
-import 'package:efashion_flutter/features/home/presentation/screens/details_screen.dart';
-import 'package:efashion_flutter/features/home/presentation/screens/all_products_screen.dart';
-import 'package:efashion_flutter/features/home/presentation/screens/notifications_screen.dart';
-import 'package:flutter/material.dart';
+import 'package:efashion_flutter/features/home/presentation/screens/see_all_screen.dart';
 import 'package:injectable/injectable.dart';
 
 part 'app_router.gr.dart';
@@ -31,16 +23,7 @@ class AppRouter extends _$AppRouter {
           children: [
             AutoRoute(
               page: SplashRoute.page,
-            ),
-            AutoRoute(
-              page: WelcomeRoute.page,
               initial: true,
-            ),
-            AutoRoute(
-              page: LoginRoute.page,
-            ),
-            AutoRoute(
-              page: SignupRoute.page,
             ),
           ],
         ),
@@ -50,36 +33,14 @@ class AppRouter extends _$AppRouter {
           children: [
             AutoRoute(
               page: HomeTabRoute.page,
-              initial: true,
               children: [
                 AutoRoute(
                   page: HomeRoute.page,
                   initial: true,
                 ),
                 CustomRoute(
-                  page: AllProductsRoute.page,
+                  page: SeeAllRoute.page,
                   transitionsBuilder: TransitionsBuilders.fadeIn,
-                  durationInMilliseconds: 300,
-                ),
-                CustomRoute(
-                  page: DetailsRoute.page,
-                  transitionsBuilder: TransitionsBuilders.fadeIn,
-                  durationInMilliseconds: 300,
-                ),
-                CustomRoute(
-                  page: AddReviewRoute.page,
-                  transitionsBuilder: TransitionsBuilders.fadeIn,
-                  durationInMilliseconds: 300,
-                ),
-                CustomRoute(
-                  page: AllReviewsRoute.page,
-                  transitionsBuilder: TransitionsBuilders.fadeIn,
-                  durationInMilliseconds: 300,
-                ),
-                CustomRoute(
-                  page: NotificationsRoute.page,
-                  transitionsBuilder: TransitionsBuilders.fadeIn,
-                  durationInMilliseconds: 300,
                 ),
               ],
             ),
