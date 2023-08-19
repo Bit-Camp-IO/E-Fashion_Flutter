036c2ae53e4b18e4c0aa46be033ae999bce92e99
Abdullah-3hmed <abdullhahmed107@gmail.com>
2023-08-19 18:36:32 +0300

    finish order status screen

--- lib/features/account/presentation/components/custom_easy_stepper.dart
+++ lib/features/account/presentation/components/custom_easy_stepper.dart
@@ -10,90 +10,129 @@ class CustomEasyStepper extends StatefulWidget {
 }
 
 class _CustomEasyStepperState extends State<CustomEasyStepper> {
-  int activeColor = 0;
+  int activeIndex = 1;
+  final List<String> orderStatusImages = const [
+    "assets/images/stepper_image1.jpg",
+    "assets/images/stepper_image2.jpg",
+    "assets/images/stepper_image3.jpg",
+  ];
+  final List<String> orderStatusMessages = const [
+    "Your order still on progress \n it may take hours",
+    "The delivery man on his \n way to your location",
+    "Your order has been \n delivered, hope you liked it",
+  ];
   @override
   Widget build(BuildContext context) {
-    return EasyStepper(
-      activeStep: 0,
-      lineLength: 100,
-      lineSpace: 0,
-      lineType: LineType.normal,
-      defaultLineColor: Theme.of(context).colorScheme.outlineVariant,
-      finishedLineColor: Colors.orange,
-      activeStepTextColor: Colors.black87,
-      finishedStepTextColor: Colors.black87,
-      internalPadding: 0,
-      showLoadingAnimation: false,
-      stepRadius: 20,
-      showStepBorder: false,
-      lineThickness: 1,
-      steps: [
-        EasyStep(
-          customStep: Container(
-            width: 26.w,
-            height: 26.h,
-            decoration: BoxDecoration(
-              shape: BoxShape.circle,
-              border: Border.all(
-                color: activeColor == 0 ? Theme.of(context).colorScheme.primary : Colors.grey[300]!,
-                width: 3.0,
-              ),
-            ),
-            child: activeColor == 0
-                ? Icon(
-                    Icons.check,
-                    size: 22.0,
-                    color: Theme.of(context).colorScheme.primary,
-                  )
-                : null,
-          ),
-          title: 'On progress',
+    return Column(
+      children: [
+        SizedBox(
+          height: 24.h,
         ),
-        EasyStep(
-          customStep: Container(
-            width: 26.w,
-            height: 26.h,
-            decoration: BoxDecoration(
-              shape: BoxShape.circle,
-              border: Border.all(
-                color: activeColor == 1 ? Theme.of(context).colorScheme.primary : Colors.grey[300]!,
-                width: 3.0,
+        EasyStepper(
+          activeStep: activeIndex,
+          lineLength: 100,
+          lineSpace: 0,
+          lineType: LineType.normal,
+          defaultLineColor: Theme.of(context).colorScheme.outlineVariant,
+          //finishedLineColor: Theme.of(context).colorScheme.primary,
+          activeStepTextColor: Colors.black87,
+          finishedStepTextColor: Theme.of(context).colorScheme.outlineVariant,
+          internalPadding: 0,
+          showLoadingAnimation: false,
+          stepRadius: 20,
+          showStepBorder: false,
+          lineThickness: 1,
+          activeStepBorderColor: Theme.of(context).colorScheme.primary,
+          activeStepIconColor: Theme.of(context).colorScheme.primary,
+          finishedStepBorderColor: Colors.grey[300],
+          finishedStepBorderType: BorderType.normal,
+          //activeLineColor: Theme.of(context).colorScheme.primary,
+          finishedStepBackgroundColor: Colors.transparent,
+          steps: [
+            EasyStep(
+              customStep: Container(
+                width: 26.w,
+                height: 26.h,
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  border: Border.all(
+                    color: activeIndex == 0 ? Theme.of(context).colorScheme.primary : Colors.grey[300]!,
+                    width: 3.0,
+                  ),
+                ),
+                child: activeIndex == 0
+                    ? Icon(
+                        Icons.check,
+                        size: 22.0,
+                        color: Theme.of(context).colorScheme.primary,
+                      )
+                    : null,
               ),
+              title: 'On progress',
             ),
-            child: activeColor == 1
-                ? Icon(
-                    Icons.check,
-                    size: 22.0,
-                    color: Theme.of(context).colorScheme.primary,
-                  )
-                : null,
-          ),
-          title: 'On its way',
-          //topTitle: true,
+            EasyStep(
+              customStep: Container(
+                width: 26.w,
+                height: 26.h,
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  border: Border.all(
+                    color: activeIndex == 1 ? Theme.of(context).colorScheme.primary : Colors.grey[300]!,
+                    width: 3.0,
+                  ),
+                ),
+                child: activeIndex == 1
+                    ? Icon(
+                        Icons.check,
+                        size: 22.0,
+                        color: Theme.of(context).colorScheme.primary,
+                      )
+                    : null,
+              ),
+              title: 'On its way',
+              //topTitle: true,
+            ),
+            EasyStep(
+              customStep: Container(
+                width: 26.w,
+                height: 26.h,
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  border: Border.all(
+                    color: activeIndex == 2 ? Theme.of(context).colorScheme.primary : Colors.grey[300]!,
+                    width: 3.0,
+                  ),
+                ),
+                child: activeIndex == 2
+                    ? Icon(
+                        Icons.check,
+                        size: 22.0,
+                        color: Theme.of(context).colorScheme.primary,
+                      )
+                    : null,
+              ),
+              title: 'Delivered',
+            ),
+          ],
+          onStepReached: (index) => setState(() => activeIndex = index),
         ),
-        EasyStep(
-          customStep: Container(
-            width: 26.w,
-            height: 26.h,
-            decoration: BoxDecoration(
-              shape: BoxShape.circle,
-              border: Border.all(
-                color: activeColor == 2 ? Theme.of(context).colorScheme.primary : Colors.grey[300]!,
-                width: 3.0,
-              ),
-            ),
-            child: activeColor == 2
-                ? Icon(
-                    Icons.check,
-                    size: 22.0,
-                    color: Theme.of(context).colorScheme.primary,
-                  )
-                : null,
-          ),
-          title: 'Delivered',
+        SizedBox(
+          height: 97.h,
+        ),
+        Image(
+          width: 134.w,
+          fit: BoxFit.cover,
+          image: AssetImage(orderStatusImages[activeIndex]),
+        ),
+        SizedBox(
+          height: 24.h,
+        ),
+        Text(
+          orderStatusMessages[activeIndex],
+          textAlign: TextAlign.center,
+          style: Theme.of(context).textTheme.bodySmall,
         ),
       ],
-      // onStepReached: (index) => setState(() => activeStep = index),
     );
   }
 }
